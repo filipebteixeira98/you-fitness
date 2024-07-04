d969c29b7ed939b3d2e834e6b812f89ef0d53f06
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-07-04 19:16:12 -0300

    chore: create home screen structure

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,55 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native'
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen'
+import { StatusBar } from 'expo-status-bar'
+import { LinearGradient } from 'expo-linear-gradient'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 export default function Home() {
   return (
-    <View>
-      <Text>You fitness</Text>
+    <View className="flex flex-1 justify-end">
+      <StatusBar style="light" />
+      <Image
+        className="h-full w-full absolute"
+        source={require('../assets/images/welcome.png')}
+      />
+      <LinearGradient
+        colors={['transparent', '#18181b']}
+        style={{ width: wp(100), height: hp(70) }}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 0.8 }}
+        className="flex justify-end pb-12 space-y-8"
+      >
+        <View className="flex items-center">
+          <Text
+            style={{ fontSize: hp(5) }}
+            className="text-white font-bold tracking-wide"
+          >
+            Best <Text className="text-rose-500">Workouts</Text>
+          </Text>
+          <Text
+            style={{ fontSize: hp(5) }}
+            className="text-white font-bold tracking-wide"
+          >
+            For You
+          </Text>
+        </View>
+        <View>
+          <TouchableOpacity
+            style={{ height: hp(7), width: wp(70) }}
+            className="flex items-center justify-center mx-auto rounded-full border-[2px] border-neutral-200 bg-rose-500"
+          >
+            <Text
+              style={{ fontSize: hp(3) }}
+              className="text-white font-bold tracking-widest"
+            >
+              Get Started
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </LinearGradient>
     </View>
-  );
+  )
 }
